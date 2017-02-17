07ed98b4a655916c928f95772d1bccfaf585c840
phrush <w.phrush@gmail.com>
2017-02-16 20:17:01 -0600

    (update) schema

--- schema.sql
+++ schema.sql
@@ -1,26 +1,48 @@
 CREATE DATABASE secrets;
 
-USE secrets;
+USE spurr;
 
-
-
-CREATE TABLE user (
+CREATE TABLE users (
   /* Describe your table here.*/
   user_id int not null auto_increment,
   username varchar(15),
+  password varchar(15),
+  location varchar(15),
   Primary key(user_id),
   UNIQUE (username)
 );
 
-CREATE TABLE messages (
+CREATE TABLE user_saved_spurrs (
   /* Describe your table here.*/
   id int not null auto_increment,
   user_id int,
-  message text,
-  room_name varchar(15),
-  Primary key(id)
-  -- foreign key(user_id) references users (user_id)
+  spurr_id int,
+  timestamp int,
+  message varchar(15),
+  location varchar(15),
+  Primary key(id),
+  foreign key(user_id) references users (user_id),
+  foreign key(spurr_id) references saved_spurrs (spurr_id)
 );
+
+CREATE TABLE saved_spurrs (
+  /* Describe your table here.*/
+  spurr_id int not null auto_increment,
+  timestamp int,
+  message varchar(15),
+  location varchar(15),
+  Primary key(spurr_id)
+);
+
+CREATE TABLE spurrs (
+  /* Describe your table here.*/
+  spurr_id int not null auto_increment,
+  timestamp int,
+  message varchar(15),
+  location varchar(15),
+  Primary key(spurr_id)
+);
+
 /* Create other tables and define schemas for them here! */
 
 /*  Execute this file from the command line by typing:
--- server-config.js
+++ server-config.js
@@ -56,17 +56,16 @@ app.get('/', (req, res) => {
 });
 
 
-//Connecting to our models ======
+// Connecting to our models ======
 // require('./models/prayerRequestModel.js'); // which executes 'mongoose.connect()'
-
 
 
 // middlewares ===================================================================
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev')); // log every request to the console
-app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: 'true' })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
 module.exports = app;
