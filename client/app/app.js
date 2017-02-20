f7e176976c3d063d51646f3627459e9004c1b8cd
phrush <w.phrush@gmail.com>
2017-02-20 16:39:51 -0600

    (add) views skeleton

--- client/app/app.js
+++ client/app/app.js
@@ -1,4 +1,18 @@
+const router = function ($routeProvider) {
+  $routeProvider
+    .when('/test', {
+      templateUrl: './views/test.html',
+      // controller: '',
+    })
+    .when('/secrets', {
+      templateUrl: './views/secrets.html',
+      // controller: '',
+    });
+};
+
 angular.module('Spurr', [
   'Spurr-Ctrl',
   'Spurr-Fact',
-]);
+  'ngRoute',
+])
+.config(router);
--- client/app/controller.js
+++ client/app/controller.js
@@ -1,6 +1,20 @@
 angular.module('Spurr-Ctrl', [])
 .controller('SpurrCtrl', function ctrl($scope, SpurrFact) {
-  this.default = '';
+  $scope.default = '';
+
+  $scope.secret = {
+    who: 'Liv',
+    when: 'Today',
+    where: 'NOLA',
+    what: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  };
+
+  $scope.secrets = [
+    $scope.secret,
+    $scope.secret,
+    $scope.secret,
+    $scope.secret,
+  ];
 
   $scope.test = function (input) {
     SpurrFact.test(input);
