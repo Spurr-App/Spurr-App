377fcad587aadc99f45ffa26ebcbe3f0d07a5133
phrush <w.phrush@gmail.com>
2017-02-27 19:36:26 -0600

    (update) documentation

--- client/app/mainCtrl.js
+++ client/app/mainCtrl.js
@@ -2,6 +2,12 @@ angular.module('Spurr-Ctrl', [])
 .controller('SpurrCtrl', function ($scope, SpurrFact, $window, $location) {
   $scope.default = '';
 
+  /**
+   * Checks the length of $window.localStorage,
+   * which determines whether or not a user is logged in.
+   * Used to alternate visible nav bar links and about page paragraphs
+   * @return {Number} 0 or 1
+   */
   $scope.user = () => $window.localStorage.length;
 
   /**
