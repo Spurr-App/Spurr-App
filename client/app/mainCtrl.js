a76a0820fff5ae85ed9261055b34c159506dbfb1
phrush <w.phrush@gmail.com>
2017-02-27 19:33:57 -0600

    (add) ng-show nav bar depending if logged in or not

--- client/app/mainCtrl.js
+++ client/app/mainCtrl.js
@@ -2,13 +2,15 @@ angular.module('Spurr-Ctrl', [])
 .controller('SpurrCtrl', function ($scope, SpurrFact, $window, $location) {
   $scope.default = '';
 
+  $scope.user = () => $window.localStorage.length;
+
   /**
    * Runs a function to console log input
    * Works on any view to ensure angular is working
    * @param {Any} input
    */
   $scope.test = function (input) {
-    SpurrFact.esc(input);
+    SpurrFact.test(input);
   };
 
   /**
