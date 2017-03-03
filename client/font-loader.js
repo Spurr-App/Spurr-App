e4ed6a7aa3bfc3227c515161d8cc76b8217ce33d
defields923 <defields923@gmail.com>
2017-03-03 16:41:10 -0600

    (feature) add an assortment of new fonts

---
+++ client/font-loader.js
@@ -0,0 +1,19 @@
+google.load('webfont','1');
+  google.setOnLoadCallback(function() {
+    WebFont.load({
+      google: {
+	      families: [
+          'Arial','Helvetica','Futura','Times','Comic Sans MS','Papyrus','Courier New',
+          'Arial Black','Impact','Earwig Factory','Jazz Ball','I Love Glitter','Andale Mono',
+          'Tangerine','Poiret One','Open Sans','Diplomata SC','Baloo','Griffy','Oswald',
+          'Montserrat','Merriweather','Rochester','Sirin Stencil','Indie Flower','Bitter',
+          'Fjalla One','Inconsolata','Dosis','Anton','Cabin','Arvo','Fascinate Inline',
+          'Vampiro One','Josefin Sans','Ravi Prakash','Mr Dafoe','Plaster','Frijole','Mogra',
+          'Faster One','Bungee','Monoton','Aladin','Rancho','Mirza','Simonetta','Farsan',
+          'Yesteryear','Lancelot','Wallpoet','Patrick Hand SC','Barrio','Rakkas',
+          'Concert One','Mrs Saint Delafield','Erica One','Almendra Display','Squada One',
+          'New Rocker','Audiowide','Chelsea Market',
+        ]
+      }
+    });
+  });
